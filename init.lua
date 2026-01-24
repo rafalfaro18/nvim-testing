fef7dd4bf8392084893e4d64c98a950ffa287ad1
Rafa <16846380+rafalfaro18@users.noreply.github.com>
2026-01-23 22:22:40 -0600

    remaps esc to C\Cn in terminal mode
    escape to exit terminal mode

--- init.lua
+++ init.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
+vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { noremap = true })
