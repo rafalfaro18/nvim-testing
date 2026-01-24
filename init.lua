cf1d859e718d344ec6b1e388c9e16bde71923b62
Rafa <16846380+rafalfaro18@users.noreply.github.com>
2026-01-24 03:05:05 -0600

    installs telescope and dependency plenary

--- init.lua
+++ init.lua
@@ -14,6 +14,8 @@ vim.pack.add({
     'https://github.com/folke/tokyonight.nvim',
     'https://github.com/nvim-tree/nvim-web-devicons',
     'https://github.com/nvim-lualine/lualine.nvim',
+    'https://github.com/nvim-lua/plenary.nvim',
+    'https://github.com/nvim-telescope/telescope.nvim',
 })
 
 require('tokyonight').setup({
