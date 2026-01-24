4c17751395d825338b325713127c97e33a019545
Rafa <16846380+rafalfaro18@users.noreply.github.com>
2026-01-23 22:57:02 -0600

    use transparent folke/tokyonight colorscheme package installed plugin

--- init.lua
+++ init.lua
@@ -1,6 +1,22 @@
 vim.opt.relativenumber = true 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.opt.termguicolors = true
 
 vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
 vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { noremap = true })
+
+vim.pack.add({
+	'https://github.com/folke/tokyonight.nvim',
+})
+
+require('tokyonight').setup({
+        transparent = true,
+        style = "night",
+	styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+        },
+})
+
+vim.cmd("colorscheme tokyonight")
