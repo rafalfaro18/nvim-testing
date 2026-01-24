9f2f29dcd3b3c24c8780d8d578e67d9d87cc5bea
Rafa <16846380+rafalfaro18@users.noreply.github.com>
2026-01-24 02:23:38 -0600

    tab 4 spaces

--- init.lua
+++ init.lua
@@ -1,22 +1,25 @@
+vim.opt.number = true 
 vim.opt.relativenumber = true 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.opt.termguicolors = true
+vim.opt.cursorline = true
+vim.opt.shiftwidth = 4
 
 vim.keymap.set('n', '<leader>w', ':w<CR>', { desc = 'Save file' })
 vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { noremap = true })
 
 vim.pack.add({
-	'https://github.com/folke/tokyonight.nvim',
+    'https://github.com/folke/tokyonight.nvim',
 })
 
 require('tokyonight').setup({
-        transparent = true,
-        style = "night",
-	styles = {
-            sidebars = "transparent",
-            floats = "transparent",
-        },
+    transparent = true,
+    style = "night",
+    styles = {
+	sidebars = "transparent",
+	floats = "transparent",
+    },
 })
 
 vim.cmd("colorscheme tokyonight")
