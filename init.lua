dcf547889631d0c4f3054279d0c7faf96abab066
Rafa <16846380+rafalfaro18@users.noreply.github.com>
2026-01-31 14:33:10 -0600

    replace native autocomplete with blink.cmp

--- init.lua
+++ init.lua
@@ -29,6 +29,7 @@ vim.pack.add({
     'https://github.com/nvim-treesitter/nvim-treesitter',
     'https://github.com/neovim/nvim-lspconfig',
     'https://github.com/windwp/nvim-autopairs',
+    'https://github.com/saghen/blink.cmp'
 })
 
 local builtin = require('telescope.builtin')
@@ -52,6 +53,10 @@ require('lualine').setup({
     theme = "tokyonight",
 })
 
+require('blink.cmp').setup({
+    fuzzy = { implementation = "prefer_rust" }
+})
+
 vim.api.nvim_create_autocmd('FileType', {
     pattern = { 'tsx', 'javascript', 'typescript', 'python' },
     callback = function()
@@ -65,40 +70,10 @@ vim.api.nvim_create_autocmd('FileType', {
     end,
 })
 
-vim.lsp.enable('lua_ls')
 vim.lsp.enable('pyright')
 vim.lsp.enable('eslint')
 vim.lsp.enable('ts_ls')
 require("nvim-autopairs").setup({})
 
-vim.api.nvim_create_autocmd('LspAttach', {
-    group = vim.api.nvim_create_augroup('my.lsp', {}),
-    callback = function(args)
-        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-        if client:supports_method('textDocument/implementation') then
-            -- Create a keymap for vim.lsp.buf.implementation ...
-        end
-
-        -- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-        if client:supports_method('textDocument/completion') then
-            -- Optional: trigger autocompletion on EVERY keypress. May be slow!
-            -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-            -- client.server_capabilities.completionProvider.triggerCharacters = chars
-
-            vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-        end
-
-        -- Auto-format ("lint") on save.
-        -- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-        if not client:supports_method('textDocument/willSaveWaitUntil')
-            and client:supports_method('textDocument/formatting') then
-            vim.api.nvim_create_autocmd('BufWritePre', {
-                group = vim.api.nvim_create_augroup('my.lsp', { clear = false }),
-                buffer = args.buf,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-                end,
-            })
-        end
-    end,
-})
+vim.lsp.config('lua_ls', { settings = { diagnostics = { globals = { "vim" } } } })
+vim.lsp.enable('lua_ls')
